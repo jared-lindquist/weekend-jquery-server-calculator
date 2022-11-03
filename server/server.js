0b891cbe4f465953483e027435e8b5acd24d9430
Jared Lindquist <jared.lindquist1985@gmail.com>
2022-11-03 15:45:15 -0500

    File structure set up, HTML skeleton set up.

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+
+app.listen(port, () => {
+    console.log('listening on port', port);
+})
+
+app.use(express.static('server/public'));
