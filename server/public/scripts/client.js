60d4c08a84703a23263a9db5619abcf6838e4d32
Jared Lindquist <jared.lindquist1985@gmail.com>
2022-11-03 19:40:59 -0500

    Added a bunch of code comments on things I think will be needed, and some framework for GET/POST that I think I will need.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,5 +2,50 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('ready');
-    
+
+    //getCalculations(); //this could be how we keep the calc history on the DOM?
+    //create click listener for the submit(=)btn
+    //create click listeners for the (+, -, *, %)btns
+    //
 }
+
+//function getCalculations() {
+    //console.log('we are getting calculations from the server);
+    //$.ajax({
+        //method: 'GET',
+        //url: /*insert route here
+    //}).then(function(response) {
+        //console.log('wahoo!', response);
+        //renderToDom(response);//should we use this?
+    //}).catch(function(error) {
+        //alert('Too bad so sad :()
+//     })
+// }
+
+//function renderToDom(do we need this?) {
+    //$('#history').empty();
+
+    //for (let output of calculations) {
+        //$('#output').append(`
+            //<li>
+                //place holder for num1, calc function, num2, output
+            //</li>
+//         `)
+//     }
+// }
+
+//function addCalculation() {
+    //$.ajax({
+    //method: 'POST',
+    //url: *insert route here
+    //data: ${num1 input}.val(),
+        //  ${calcFunction} how do we capture this?,
+        //  ${num2 input}.val(),
+        //  ${output}.val()
+    // }).then(function(response) {
+        //console.log('very nice');
+        //getCalculations(); //Must get updated data from server
+    // }).catch(function(error) {
+        //alert('not good', error)
+    // })
+// }
--- server/server.js
+++ server/server.js
@@ -2,9 +2,37 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+//do we need to add a module to hold the calculation history in?
+// let inventory = require('./modules/*insert route here')
+
+let bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static('server/public'));
+
+
+//app.get(/*insert route here, (req, res) => {
+    // console.log('request for *insert route here was made');
+//send back calculation history items
+//so we can see it in the browser
+
+//res.send(*insert module here) **if we use a module to store the 
+//calculation history
+//res.sendStatus(200);
+// })
+
+//app.post('/*insert route here, (req, res) => {
+
+//console.log('we are posting', req.body);
+
+//let item = req.body;
+//inventory.push(item);
+
+//send this response in every post request
+//res.sendStatus(200);
+// })
 
 app.listen(port, () => {
     console.log('listening on port', port);
 })
 
-app.use(express.static('server/public'));
